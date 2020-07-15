d19ac725ffb602139306f1a669b41091280c969b
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-15 15:09:09 +0530

    two input boxes

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,31 +4,36 @@ import React, { Component } from 'react';
 
 class Form extends Component{
   state={
-      value:"",
-      final:""
+      first:"",
+      second:""
   }
 
   
     handleChnage=(e)=>{
 
-this.setState({value:e.target.value})
+this.setState({first:e.target.value})
+this.setState({second:e.target.value})
 }
 
 finalSubmit=(e)=>{
 e.preventDefault();
     this.setState({final:this.state.value})
+
 }
    
    
     render(){
         return(
 <form onSubmit={this.finalSubmit}>
-<input type="text" onChange={this.handleChnage}/>
+
+<input type="text" value={this.state.first} onChange={this.handleChnage}/>
+<input type="text" value={this.state.second} onChange={this.handleChnage}/>
+
 <button >
     click mee
 
 </button>
-<h1>{this.state.final}</h1>
+
 </form>
 
            
